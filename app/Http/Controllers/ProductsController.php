bb2ee5f493cac1b2a27852ee7cc9c3ba5e7690cd
ffadelm <faysalfadelm0107@gmail.com>
2023-07-28 20:18:27 +0700

    feat: HTTP Req untuk data Products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,67 +2,134 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\products;
-use App\Http\Requests\StoreproductsRequest;
-use App\Http\Requests\UpdateproductsRequest;
+use App\Models\Products;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Resources\ProductsResource;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        $product = products::all();
-        return ProductsResource::collection($product);
-    }
+        // Ambil semua data produk dari database
+        $products = Products::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // Return data dalam bentuk JSON menggunakan resource ProductsResource
+        return ProductsResource::collection($products);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreproductsRequest $request)
+    public function store(Request $request)
     {
-        //
+        // Validasi data yang diterima dari request menggunakan Validator
+        $validator = Validator::make($request->all(), [
+            'kode' => 'required|string',
+            'nama' => 'required|string',
+            'harga' => 'required|numeric',
+            'gambar' => 'required|image|mimes:jpg,png,jpeg,heic,heif,webp|max:2048',
+        ]);
+
+        // Jika validasi gagal, kembalikan respons JSON dengan pesan error
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        // Jika validasi berhasil, simpan data ke database
+        $photo = $request->file('gambar');
+        $photo->storeAs('public/images', $photo->hashName());
+
+        Products::create([
+            'kode' => $request->kode,
+            'nama' => $request->nama,
+            'harga' => $request->harga,
+            'gambar' => $photo->hashName(),
+        ]);
+
+        // Kembalikan respons JSON dengan pesan sukses
+        return response()->json(['message' => 'produk berhasil ditambahkan'], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Products  $product
+     * @return \App\Http\Resources\ProductsResource
      */
-    public function show(products $products)
+    public function show(Products $product)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(products $products)
-    {
-        //
+        // Return data produk dalam bentuk JSON menggunakan resource ProductsResource
+        return new ProductsResource($product);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(UpdateproductsRequest $request, products $products)
+    public function update(Request $request, Products $product)
     {
-        //
+        // Validasi data yang diterima dari request menggunakan Validator
+        $validator = Validator::make($request->all(), [
+            'kode' => 'required|string',
+            'nama' => 'required|string',
+            'harga' => 'required|numeric',
+            'gambar' => 'image|mimes:jpg,png,jpeg,heic,heif,webp|max:2048',
+        ]);
+
+        // Jika validasi gagal, kembalikan respons JSON dengan pesan error
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+
+        // Jika ada file gambar yang diunggah, simpan gambar baru dan hapus gambar lama
+        if ($request->hasFile('gambar')) {
+            $photo = $request->file('gambar');
+            $photo->storeAs('public/images', $photo->hashName());
+
+            Storage::delete('public/images/' . $product->gambar);
+
+            $product->gambar = $photo->hashName();
+        }
+
+        // Update data produk dengan data baru dari request
+        $product->kode = $request->kode;
+        $product->nama = $request->nama;
+        $product->harga = $request->harga;
+        $product->save();
+
+        // Kembalikan respons JSON dengan pesan sukses
+        return response()->json(['message' => 'Data berhasil diupdate'], Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Products  $product
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(products $products)
+    public function destroy(Products $product)
     {
-        //
+        // Hapus gambar terkait dari penyimpanan
+        Storage::delete('public/images/' . $product->gambar);
+
+        // Hapus data produk dari database
+        $product->delete();
+
+        // Kembalikan respons JSON dengan pesan sukses
+        return response()->json(['message' => 'produk berhasil dihapus'], Response::HTTP_OK);
     }
 }
